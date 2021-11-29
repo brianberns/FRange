b6085cfcd262fcd9f0e9ca1f770aa1b999bd60ea
Brian Berns <brianberns@gmail.com>
2021-11-29 12:37:26 -0500

    More work on union.

--- FRangeTests/Tests.fs
+++ FRangeTests/Tests.fs
@@ -5,11 +5,21 @@ namespace FRange
 open FsCheck
 open FsCheck.Xunit
 
+module Int =
+    let gen = Arb.from<int>.Generator
+
+type Triplet =
+    {
+        AOpt : Option<int>
+        B : int
+        COpt : Option<int>
+    }
+
 type Generators =
 
     static member Range() =
         let genBoundOpt =
-            Arb.from<int>.Generator
+            Int.gen
                 |> Gen.apply (Gen.elements [ Inclusive; Exclusive ])
                 |> Gen.optionOf
         let rec genRange =
@@ -21,6 +31,25 @@ type Generators =
                     | None -> return! genRange
             }
         genRange |> Arb.fromGen
+
+    static member Triplet() =
+        gen {
+            let! aOpt = Int.gen |> Gen.optionOf
+            let! b =
+                Int.gen
+                    |> Gen.where (fun b ->
+                        aOpt
+                            |> Option.map (fun a -> b > a)
+                            |> Option.defaultValue true)
+            let! cOpt =
+                Int.gen
+                    |> Gen.optionOf
+                    |> Gen.where (fun cOpt ->
+                        cOpt
+                            |> Option.map (fun c -> c > b)
+                            |> Option.defaultValue true)
+            return { AOpt = aOpt; B = b; COpt = cOpt }
+        } |> Arb.fromGen
 
 module Tests =
 
@@ -67,6 +96,20 @@ module Tests =
         let union0 = Range.union (rangesAB @ [ rangeC ])
         let union1 = Range.union (rangeA :: rangesBC)
         union0 = union1
+
+    [<Property>]
+    let ``Merge adjancent ranges`` triplet =
+        let boundAOpt = triplet.AOpt |> Option.map Inclusive
+        let boundCOpt = triplet.COpt |> Option.map Inclusive
+        let rangeABIncl = Range.create boundAOpt (Some (Inclusive triplet.B))
+        let rangeABExcl = Range.create boundAOpt (Some (Exclusive triplet.B))
+        let rangeBCIncl = Range.create (Some (Inclusive triplet.B)) boundCOpt
+        let rangeBCExcl = Range.create (Some (Exclusive triplet.B)) boundCOpt
+        let rangeAC = Range.create boundAOpt boundCOpt
+        Range.union2 rangeABIncl rangeBCIncl = [ rangeAC ]
+            && Range.union2 rangeABIncl rangeBCExcl = [ rangeAC ]
+            && Range.union2 rangeABExcl rangeBCIncl = [ rangeAC ]
+            && Range.union2 rangeABExcl rangeBCExcl = [ rangeABExcl; rangeBCExcl ]
 
     [<assembly: Properties(
         Arbitrary = [| typeof<Generators> |],
