6f5eaabbf4bd22b2c47cab14e3a8b4807659550b
Brian Berns <brianberns@gmail.com>
2021-12-01 01:30:44 -0500

    Fixed compiler error.

--- FRangeTests/Program.fs
+++ FRangeTests/Program.fs
@@ -4,7 +4,4 @@ module Program =
 
     [<EntryPoint>] 
     let main args =
-        let ranges = [ !*- 0 ; !-* -1 ]
-        ``Difference tests``.``Double inverse is identity`` ranges
-            |> printfn "%A"
         0
