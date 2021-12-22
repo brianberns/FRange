e25b64e4202adc87a91282d50e056d6a8fc8f035
Brian Berns <brianberns@gmail.com>
2021-12-22 01:58:31 -0500

    Added namespace to appease test environment.

--- FRangeCSharpTests/Tests.cs
+++ FRangeCSharpTests/Tests.cs
@@ -1,72 +1,73 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using FRange.CSharp;
-using Range = FRange.CSharp.Range;   // resolve conflict with System.Range
 
-[TestClass]
-public class Tests
+namespace FRange.CSharp
 {
-    private void AssertEquivalent<T>(MultiRange<T> mrA, MultiRange<T> mrB) where T : IComparable<T>
-    {   
-        Assert.IsTrue(mrA.IsEquivalent(mrB), mrB.ToString());
-    }
-
-    [TestMethod]
-    public void MultiRanges()
+    [TestClass]
+    public class Tests
     {
-        var rangeA = Range.Create(1, BoundType.Inclusive, 3, BoundType.Inclusive);
-        Assert.IsTrue(rangeA.HasLowerBound());
-        Assert.AreEqual(3, rangeA.UpperBoundValue());
-        Assert.AreEqual(BoundType.Inclusive, rangeA.UpperBoundType());
+        private void AssertEquivalent<T>(MultiRange<T> mrA, MultiRange<T> mrB) where T : IComparable<T>
+        {
+            Assert.IsTrue(mrA.IsEquivalent(mrB), mrB.ToString());
+        }
 
-        var mrA = rangeA.ToMultiRange();
-        var mrB = Range.Create(2, BoundType.Exclusive, 4, BoundType.Exclusive).ToMultiRange();
+        [TestMethod]
+        public void MultiRanges()
+        {
+            var rangeA = Range.Create(1, BoundType.Inclusive, 3, BoundType.Inclusive);
+            Assert.IsTrue(rangeA.HasLowerBound());
+            Assert.AreEqual(3, rangeA.UpperBoundValue());
+            Assert.AreEqual(BoundType.Inclusive, rangeA.UpperBoundType());
 
-        var union = Range.Create(1, BoundType.Inclusive, 4, BoundType.Exclusive).ToMultiRange();
-        AssertEquivalent(union, mrA.Union(mrB));
+            var mrA = rangeA.ToMultiRange();
+            var mrB = Range.Create(2, BoundType.Exclusive, 4, BoundType.Exclusive).ToMultiRange();
 
-        var intersection = Range.Create(2, BoundType.Exclusive, 3, BoundType.Inclusive).ToMultiRange();
-        AssertEquivalent(intersection, mrA.Intersection(mrB));
+            var union = Range.Create(1, BoundType.Inclusive, 4, BoundType.Exclusive).ToMultiRange();
+            AssertEquivalent(union, mrA.Union(mrB));
 
-        var inverse =
-            MultiRange.Create(new[]
-                {
-                    Range.CreateUpper(1, BoundType.Exclusive),
-                    Range.CreateLower(4, BoundType.Inclusive)
-                });
-        AssertEquivalent(inverse, union.Inverse());
-        Assert.IsTrue(inverse.Contains(-1));
-    }
+            var intersection = Range.Create(2, BoundType.Exclusive, 3, BoundType.Inclusive).ToMultiRange();
+            AssertEquivalent(intersection, mrA.Intersection(mrB));
 
-    static void Main()
-    {
-        var aSchedule =
-            MultiRange.Create(
-                new[] {
-                    Range.Create(
-                        DateTime.Parse("11:30am"), BoundType.Inclusive,
-                        DateTime.Parse("1pm"), BoundType.Exclusive),
-                    Range.Create(
-                        DateTime.Parse("2:30pm"), BoundType.Inclusive,
-                        DateTime.Parse("4:30pm"), BoundType.Exclusive)
-                });
-        var bSchedule =
-            MultiRange.Create(
-                new[] {
-                    Range.Create(
-                        DateTime.Parse("10am"), BoundType.Inclusive,
-                        DateTime.Parse("11am"), BoundType.Exclusive),
-                    Range.Create(
-                        DateTime.Parse("11am"), BoundType.Inclusive,
-                        DateTime.Parse("3pm"), BoundType.Exclusive)
-                });
-        var day =
-            Range.Create(
-                DateTime.Parse("9am"), BoundType.Inclusive,
-                DateTime.Parse("5pm"), BoundType.Exclusive)
-                .ToMultiRange();
-        var free =
-            day.Difference(aSchedule.Union(bSchedule));
-        Console.WriteLine(free);
+            var inverse =
+                MultiRange.Create(new[]
+                    {
+                        Range.CreateUpper(1, BoundType.Exclusive),
+                        Range.CreateLower(4, BoundType.Inclusive)
+                    });
+            AssertEquivalent(inverse, union.Inverse());
+            Assert.IsTrue(inverse.Contains(-1));
+        }
+
+        static void Main()
+        {
+            var aSchedule =
+                MultiRange.Create(
+                    new[] {
+                        Range.Create(
+                            DateTime.Parse("11:30am"), BoundType.Inclusive,
+                            DateTime.Parse("1pm"), BoundType.Exclusive),
+                        Range.Create(
+                            DateTime.Parse("2:30pm"), BoundType.Inclusive,
+                            DateTime.Parse("4:30pm"), BoundType.Exclusive)
+                    });
+            var bSchedule =
+                MultiRange.Create(
+                    new[] {
+                        Range.Create(
+                            DateTime.Parse("10am"), BoundType.Inclusive,
+                            DateTime.Parse("11am"), BoundType.Exclusive),
+                        Range.Create(
+                            DateTime.Parse("11am"), BoundType.Inclusive,
+                            DateTime.Parse("3pm"), BoundType.Exclusive)
+                    });
+            var day =
+                Range.Create(
+                    DateTime.Parse("9am"), BoundType.Inclusive,
+                    DateTime.Parse("5pm"), BoundType.Exclusive)
+                    .ToMultiRange();
+            var free =
+                day.Difference(aSchedule.Union(bSchedule));
+            Console.WriteLine(free);
+        }
     }
 }
